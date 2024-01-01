1b86208247276587675b7e5a9dee2419b4ad9ca2
Zubair Farooqui <zubairfarooqui.here@gmail.com>
2024-01-02 01:17:15 +0530

    add story images

--- src/components/HomePage/ModalStories/ModalStories.js
+++ src/components/HomePage/ModalStories/ModalStories.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState, useCallback } from "react";
+import Stories from "react-insta-stories";
 import { animate } from "framer-motion";
 import classes from "./ModalStories.module.scss";
 
 const DataTemp = [0, 1, 2, 3, 4, 5, 6];
-
 function ModalStories(props) {
   const [selected, setSelected] = useState(3);
   const hideDiv = { fromRight: DataTemp.length - 2, fromLeft: 3 };
   const stopRight = {
-    far: hideDiv.fromRight <= selected+1, //Add +1 in selected, because value is added in setSelected after moveRight func is completed in executing.
-    next: hideDiv.fromRight < selected+1,
+    far: hideDiv.fromRight <= selected + 1, //Add +1 in selected, because value is added in setSelected after moveRight func is completed in executing.
+    next: hideDiv.fromRight < selected + 1,
   };
   const stopLeft = {
-    far: selected+1 <= 3,
-    next: selected+1 < 3,
+    far: selected + 1 <= 3,
+    next: selected + 1 < 3,
   };
 
   const [story1, setStory1] = useState(<div></div>);
@@ -22,6 +22,7 @@ function ModalStories(props) {
   const [story4, setStory4] = useState(<div></div>);
   const [story5, setStory5] = useState(<div></div>);
   const [story6, setStory6] = useState(<div></div>);
+  //const [stories, setStories] = useState(storiesa);
   const [divsInfo, setDivsInfo] = useState({
     LeftFar: { id: "story1", show: true, func: setStory1 },
     LeftNext: { id: "story2", show: true, func: setStory2 },
@@ -30,7 +31,7 @@ function ModalStories(props) {
     RightFar: { id: "story5", show: true, func: setStory5 },
     overFlowDiv: { id: "story6", show: true, func: setStory6 },
   });
-
+  const [CenterHere, setCenterHere] = useState("story3");
   const mainStoryPos = 50;
   const moveNext = mainStoryPos + 25;
   const moveNextFar = moveNext + 15;
@@ -50,11 +51,11 @@ function ModalStories(props) {
 
   const selectedUp = async (up) => {
     if (up) {
-      divsInfo.overFlowDiv.func(<div>{DataTemp[selected - 3]}</div>);
+      divsInfo.overFlowDiv.func(returnHtml(DataTemp[selected - 3], divsInfo.overFlowDiv.id));
       setSelected(selected - 1);
     } else {
-      const getValue = selected + 3;
-      divsInfo.overFlowDiv.func(<div>{DataTemp[getValue]}</div>);
+      //const getValue = selected + 3;
+      divsInfo.overFlowDiv.func(returnHtml(DataTemp[selected + 3], divsInfo.overFlowDiv.id));
       setSelected(selected + 1);
     }
   };
@@ -66,55 +67,56 @@ function ModalStories(props) {
     const getLeftFar = divsInfo.LeftFar;
     const getRightFar = divsInfo.RightFar;
     const overFlowDiv = divsInfo.overFlowDiv;
-    console.log('selected: '+ selected);
     if (selected + 1 === DataTemp.length && MR === false) {
       return;
     }
     if (selected === 0 && MR === true) {
       return;
-    }  
+    }
     await animate("#" + overFlowDiv.id, {
       display: "none",
       left: MR ? overFlow.left : overFlow.right,
     });
 
     if (MR) {
-      animate("#" + getRightFar.id, { left: overFlow.right });      
+      animate("#" + getRightFar.id, { left: overFlow.right });
       if (!(hideDiv.fromRight <= selected - 1)) {
         animate("#" + getRightNext.id, { left: moveNextFar + "%" });
       }
-      if(!stopLeft.next){
-       animate("#" + getLeftFar.id, { left: movePrevious + "%" });
+      if (!stopLeft.next) {
+        animate("#" + getLeftFar.id, { left: movePrevious + "%" });
       }
     } else {
       animate("#" + getLeftFar.id, { left: overFlow.left });
       if (!stopRight.next) {
         animate("#" + getRightFar.id, { left: moveNext + "%" });
       }
-      if (selected + 1 >= hideDiv.fromLeft-1) {
+      if (selected + 1 >= hideDiv.fromLeft - 1) {
         animate("#" + getLeftNext.id, { left: movePreviousFar + "%" });
       }
     }
 
-    const abc = MR ? moveNext + "%" : movePrevious + "%";
+    const move = MR ? moveNext + "%" : movePrevious + "%";
     animate("#" + getCenterDiv.id, {
       height: normalStory.height,
       width: normalStory.width,
-      left: abc,
+      left: move,
+      zIndex: 5,
     });
 
-    const xyz = MR ? getLeftNext.id : getRightNext.id;
-    animate("#" + xyz, {
+    const ID = MR ? getLeftNext.id : getRightNext.id;
+    animate("#" + ID, {
       height: mainStory.height,
       width: mainStory.width,
       left: mainStoryPos + "%",
+      zIndex: 10,
     });
-    
+
     if ((!stopRight.far || MR === true) && (!stopLeft.far || MR === false)) {
-      const qty = MR ? movePreviousFar + "%" : moveNextFar + "%";
+      const moveFar = MR ? movePreviousFar + "%" : moveNextFar + "%";
       animate("#" + overFlowDiv.id, {
         display: "block",
-        left: qty,
+        left: moveFar,
       });
     }
 
@@ -127,6 +129,7 @@ function ModalStories(props) {
         RightFar: { ...getRightNext },
         overFlowDiv: { ...getRightFar },
       };
+      setCenterHere( getLeftNext.id);
       selectedUp(true);
       setDivsInfo(newObj);
     } else {
@@ -138,95 +141,60 @@ function ModalStories(props) {
         RightFar: { ...overFlowDiv },
         overFlowDiv: { ...getLeftFar },
       };
+      setCenterHere(getRightNext.id);
       selectedUp(false);
       setDivsInfo(newObj);
     }
   };
 
-  // const moveRight = async (MR) => {
-  //   const getCenterDiv = divsInfo.CenterDiv;
-  //   const getLeftNext = divsInfo.LeftNext;
-  //   const getRightNext = divsInfo.RightNext;
-  //   const getLeftFar = divsInfo.LeftFar;
-  //   const getRightFar = divsInfo.RightFar;
-  //   const overFlowDiv = divsInfo.overFlowDiv;
+  const returnHtml = useCallback((index, id) => {
+    console.log(id === CenterHere);
+    console.log('id: '+id);
+    console.log('CenterHere: '+CenterHere);
+    const con = id === CenterHere;
+    let stories = [
+      {
+        url: `https://picsum.photos/750/${1255 + index}`,
+        duration: con ? 5000 : 1,
+        header: {
+          heading: con ? 'true': 'false',
+          subheading: "Posted 30m ago",
+          profileImage: "https://picsum.photos/100/100",
+        },
+      },
+      {
+        url: `https://picsum.photos/750/${1255 + index+ 1}`,
+        duration: con ? 5000 : 1,
+        header: {
+          heading: con ? 'true': 'false',
+          subheading: "Posted 30m ago",
+          profileImage: "https://picsum.photos/100/100",
+        },
+      },
+    ];
+    return (
+      <div className={classes.story__img}>
+        <Stories loop={con}  stories={stories} width={"100%"} height={"100%"} />
+      </div>
+    );
+  }, [CenterHere]);
 
-  //   // if(rightHideOverflow) {
-  //   //   animate("#" + overFlowDiv.id, {
-  //   //     backgroundColor: 'transparent',
-  //   //   });
-  //   // }
-  //   await animate("#" + overFlowDiv.id, {
-  //     display: "none",
-  //     left: MR ? overFlow.left : overFlow.right,
-  //   });
-  //   if (MR) {
-  //     animate("#" + getRightFar.id, { left: overFlow.right });
-  //     animate("#" + getRightNext.id, { left: moveNextFar + "%" });
-  //     animate("#" + getLeftFar.id, { left: movePrevious + "%" });
-  //   } else {
-  //     animate("#" + getLeftFar.id, { left: overFlow.left });
-  //     animate("#" + getLeftNext.id, { left: movePreviousFar + "%" });
-  //     animate("#" + getRightFar.id, { left: moveNext + "%" });
-  //   }
-
-  //   const abc = MR ? moveNext + "%" : movePrevious + "%";
-  //   animate("#" + getCenterDiv.id, {
-  //     height: normalStory.height,
-  //     width: normalStory.width,
-  //     left: abc,
-  //   });
-
-  //   const xyz = MR ? getLeftNext.id : getRightNext.id;
-  //   animate("#" + xyz, {
-  //     height: mainStory.height,
-  //     width: mainStory.width,
-  //     left: mainStoryPos + "%",
-  //   });
-
-  //   const qty = MR ? movePreviousFar + "%" : moveNextFar + "%";
-  //   animate("#" + overFlowDiv.id, {
-  //     display: "block",
-  //     left: qty,
-  //   });
-
-  //   if (MR) {
-  //     const newObj = {
-  //       LeftFar: { ...overFlowDiv },
-  //       LeftNext: { ...getLeftFar },
-  //       CenterDiv: { ...getLeftNext },
-  //       RightNext: { ...getCenterDiv },
-  //       RightFar: { ...getRightNext },
-  //       overFlowDiv: { ...getRightFar },
-  //     };
-  //     selectedUp(true);
-  //     setDivsInfo(newObj);
-  //   } else {
-  //     const newObj = {
-  //       LeftFar: { ...getLeftNext },
-  //       LeftNext: { ...getCenterDiv },
-  //       CenterDiv: { ...getRightNext },
-  //       RightNext: { ...getRightFar },
-  //       RightFar: { ...overFlowDiv },
-  //       overFlowDiv: { ...getLeftFar },
-  //     };
-  //     selectedUp(false);
-  //     setDivsInfo(newObj);
-  //   }
-  // };
+  const addElement = useCallback(() => {
+    divsInfo.LeftFar.func(returnHtml(DataTemp[selected - 2], divsInfo.LeftFar.id));
+    divsInfo.LeftNext.func(returnHtml(DataTemp[selected - 1], divsInfo.LeftNext.id));
+    divsInfo.CenterDiv.func(returnHtml(DataTemp[selected], divsInfo.CenterDiv.id));
+    divsInfo.RightNext.func(returnHtml(DataTemp[selected + 1], divsInfo.RightNext.id));
+    divsInfo.RightFar.func(returnHtml(DataTemp[selected + 2], divsInfo.RightFar.id));
+  }, [selected, divsInfo, returnHtml]);
 
   useEffect(() => {
     document.body.style.overflowY = "hidden";
     showAllDiv();
-    divsInfo.LeftFar.func(<div>{DataTemp[selected - 2]}</div>);
-    divsInfo.LeftNext.func(<div>{DataTemp[selected - 1]}</div>);
-    divsInfo.CenterDiv.func(<div>{DataTemp[selected]}</div>);
-    divsInfo.RightNext.func(<div>{DataTemp[selected + 1]}</div>);
-    divsInfo.RightFar.func(<div>{DataTemp[selected + 2]}</div>);
+    addElement();
     return () => {
       document.body.style.overflowY = "scroll";
     };
-  }, [showAllDiv, divsInfo, selected]);
+  }, [showAllDiv, divsInfo, addElement]);
 
   return (
     <div className={classes.modal}>
@@ -245,32 +213,32 @@ function ModalStories(props) {
         Go Right
       </button>
       {story1 && (
-        <div id="story1" className={classes.story1}>
+        <div id="story1" className={`${classes.st} ${classes.story1}`}>
           {story1}
         </div>
       )}
       {story2 && (
-        <div id="story2" className={classes.story2}>
+        <div id="story2" className={`${classes.st} ${classes.story2}`}>
           {story2}
         </div>
       )}
       {story3 && (
-        <div id="story3" className={classes.story3}>
+        <div id="story3" className={`${classes.st} ${classes.story3}`}>
           {story3}
         </div>
       )}
       {story4 && (
-        <div id="story4" className={classes.story4}>
+        <div id="story4" className={`${classes.st} ${classes.story4}`}>
           {story4}
         </div>
       )}
       {story5 && (
-        <div id="story5" className={classes.story5}>
+        <div id="story5" className={`${classes.story5}`}>
           {story5}
         </div>
       )}
       {story6 && (
-        <div id="story6" className={classes.story6}>
+        <div id="story6" className={`${classes.story6}`}>
           {story6}
         </div>
       )}
