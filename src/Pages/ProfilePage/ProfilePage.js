6017aaed8136ce49f2f4d75c91f0c9b2b5b31205
Zubair Farooqui <zubairfarooqui.here@gmail.com>
2024-01-30 19:59:11 +0530

    add dark theme to profile page

--- src/Pages/ProfilePage/ProfilePage.js
+++ src/Pages/ProfilePage/ProfilePage.js
@@ -2,9 +2,11 @@ import React from "react";
 import classes from "./ProfilePage.module.scss";
 
 import Profile from "../../components/ProfilePage/Profile/Profile";
+import { useSelector } from "react-redux";
 function ProfilePage() {
+  const weninfo = useSelector((state) => state.weninfo);
   return (
-    <div className={classes.profilePage}>
+    <div data-theme={weninfo.darkMode ? "dark" : "light"} className={classes.profilePage}>
       <Profile />
     </div>
   );
